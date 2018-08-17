a33a036bd016699773b40ea7fabce0ec3baccebb
Ricky Kwan <rickyk.162@gmail.com>
2018-08-17 03:06:59 +0000

    Initial openresty

---
+++ nginx/docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+envsubst '$AGGREGATE_HOSTNAME' < /etc/nginx/templates_conf.d/default.conf.tmpl > /etc/nginx/conf.d/default.conf
+envsubst '$HOSTED_HOSTNAME' < /etc/nginx/templates_conf.d/hosted.conf.tmpl > /etc/nginx/conf.d/hosted.conf
+envsubst '$PROXY_HOSTNAME' < /etc/nginx/templates_conf.d/proxy.conf.tmpl > /etc/nginx/conf.d/proxy.conf
+
+exec /usr/bin/openresty -g "daemon off;"
+
