fa6c464159000bc651c2c45de0320e618f5b56e3
Ricky Kwan <rickyk.162@gmail.com>
2018-08-17 03:52:19 +0000

    Basic nginx config to proxy pass to hosted and proxy registry

--- nginx/docker/entrypoint.sh
+++ nginx/docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: warn if these environment variables are not set
+
 envsubst '$AGGREGATE_HOSTNAME' < /etc/nginx/templates_conf.d/default.conf.tmpl > /etc/nginx/conf.d/default.conf
 envsubst '$HOSTED_HOSTNAME' < /etc/nginx/templates_conf.d/hosted.conf.tmpl > /etc/nginx/conf.d/hosted.conf
 envsubst '$PROXY_HOSTNAME' < /etc/nginx/templates_conf.d/proxy.conf.tmpl > /etc/nginx/conf.d/proxy.conf
